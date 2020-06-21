11b3c33ee904999f7da812f4965c9548b38d96e0
Joshua Malburg <joshua.malburg@colorado.edu>
2020-06-21 15:58:00 -0400

    added test #11

--- hw/wk2/Feasibility/feasibility_tests.c
+++ hw/wk2/Feasibility/feasibility_tests.c
@@ -50,6 +50,10 @@ U32_T ex10_period[] = {2, 5, 7, 14};
 U32_T ex10_wcet[] = {1, 1, 1, 2};
 uint32_t ex10_numSer = sizeof(ex10_period) / sizeof(U32_T);
 
+U32_T ex11_period[] = {3, 6, 9};
+U32_T ex11_wcet[] = {1, 2, 3};
+uint32_t ex11_numSer = sizeof(ex11_period) / sizeof(U32_T);
+
 int completion_time_feasibility(U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[]);
 int scheduling_point_feasibility(U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[]);
 void print_test(uint32_t numServices, uint32_t *pPeriod, uint32_t *pWcet, uint8_t exNum, 
@@ -62,10 +66,10 @@ int main(void)
 
   printf("******** Completion Test Feasibility Example\n");
 
-  #define NUM_TST 11
-  U32_T *period[] = {  ex0_period, ex1_period, ex2_period, ex3_period, ex4_period, ex5_period, ex6_period, ex7_period, ex8_period, ex9_period, ex10_period};
-  U32_T *wcet[]   = {  ex0_wcet,   ex1_wcet,   ex2_wcet,   ex3_wcet,   ex4_wcet,   ex5_wcet,   ex6_wcet,   ex7_wcet,   ex8_wcet,   ex9_wcet,   ex10_wcet};
-  uint32_t num[]  = {  ex0_numSer, ex1_numSer, ex2_numSer, ex3_numSer, ex4_numSer, ex5_numSer, ex6_numSer, ex7_numSer, ex8_numSer, ex9_numSer, ex10_numSer};
+  #define NUM_TST 12
+  U32_T *period[] = {  ex0_period, ex1_period, ex2_period, ex3_period, ex4_period, ex5_period, ex6_period, ex7_period, ex8_period, ex9_period, ex10_period, ex11_period};
+  U32_T *wcet[]   = {  ex0_wcet,   ex1_wcet,   ex2_wcet,   ex3_wcet,   ex4_wcet,   ex5_wcet,   ex6_wcet,   ex7_wcet,   ex8_wcet,   ex9_wcet,   ex10_wcet, ex11_wcet};
+  uint32_t num[]  = {  ex0_numSer, ex1_numSer, ex2_numSer, ex3_numSer, ex4_numSer, ex5_numSer, ex6_numSer, ex7_numSer, ex8_numSer, ex9_numSer, ex10_numSer, ex11_numSer};
   for(int testInd = 0; testInd < NUM_TST; ++testInd)
   {
     print_test(num[testInd], period[testInd], wcet[testInd], testInd, completion_time_feasibility);
